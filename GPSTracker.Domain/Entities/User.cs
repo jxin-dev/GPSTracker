f3e5277d474396e833e361f4851d5b794099fd8e
Jayson <oliciajayson@gmail.com>
2025-04-17 16:38:39 +0800

    add static Create method to User model

--- GPSTracker.Domain/Entities/User.cs
+++ GPSTracker.Domain/Entities/User.cs
@@ -8,5 +8,18 @@
         public string PasswordHash { get; private set; } = string.Empty;
         public DateTime CreateAt { get; private set; } = DateTime.UtcNow;
 
+        private User(Guid id, string username, string email, string passwordHash)
+        {
+            Id = id;
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+        }
+
+        public static User Create(string username, string email, string passwordHash)
+        {
+            User user = new(Guid.NewGuid(), username, email, passwordHash);
+            return user;
+        }
     }
 }
