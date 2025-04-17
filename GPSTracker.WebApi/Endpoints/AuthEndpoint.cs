7b84869da72d733e16f0055926911074c09f9784
Jayson <oliciajayson@gmail.com>
2025-04-17 19:39:57 +0800

    add AuthEndpoint using RegisterUser CQRS feature

---
+++ GPSTracker.WebApi/Endpoints/AuthEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using GPSTracker.Application.Features.Users.Commands.RegisterUser;
+using GPSTracker.Contracts.Requests;
+using GPSTracker.WebApi.Abstractions;
+using Mapster;
+using MediatR;
+
+namespace GPSTracker.WebApi.Endpoints
+{
+    public class AuthEndpoint : IEndpoint
+    {
+        public void MapEndpoints(IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("/api/auth/");
+
+            group.MapPost("/register", async (ISender sender, RegisterUserRequest request) =>
+            {
+                var command = request.Adapt<RegisterUserCommand>();
+                var result = await sender.Send(command);
+                return Results.Ok(result);
+            });
+        }
+    }
+}
