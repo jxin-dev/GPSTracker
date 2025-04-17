d70cf65c3f25b323c544f296e0cdaf86799c2e36
Jayson <oliciajayson@gmail.com>
2025-04-17 20:16:20 +0800

    implement RegisterUserValidator with FluentValidation

---
+++ GPSTracker.Application/Features/Users/Commands/RegisterUser/RegisterUserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace GPSTracker.Application.Features.Users.Commands.RegisterUser
+{
+    public class RegisterUserValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(x => x.Username).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+
+        }
+    }
+}
