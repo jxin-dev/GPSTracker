be470994614dd7b7f13f7ad62963fc0a78bcc45a
Jayson <oliciajayson@gmail.com>
2025-04-17 21:24:19 +0800

    create PagedResult and PagedParams for pagination support

---
+++ GPSTracker.Contracts/Pagination/PaginationParams.cs
@@ -0,0 +1,14 @@
+﻿namespace GPSTracker.Contracts.Pagination
+{
+    public class PaginationParams
+    {
+        private const int MaxPageSize = 100;
+        public int PageNumber { get; init; } = 1;
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            init => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+    }
+}
