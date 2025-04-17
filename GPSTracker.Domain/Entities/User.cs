6fa4ec35201f0134b5cac1d4238c7916e911f6c5
Jayson <oliciajayson@gmail.com>
2025-04-17 16:11:21 +0800

    Add user model

---
+++ GPSTracker.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace GPSTracker.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Username { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public string PasswordHash { get; private set; } = string.Empty;
+        public DateTime CreateAt { get; private set; } = DateTime.UtcNow;
+
+    }
+}
