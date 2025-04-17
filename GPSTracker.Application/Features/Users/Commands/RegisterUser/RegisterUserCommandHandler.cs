8e2a8eadbb210bd46ef6230a55c66e7d25795b76
Jayson <oliciajayson@gmail.com>
2025-04-17 19:38:28 +0800

    implement RegisterUserCommandHandler with IUserRepository and Mapster for object mapping

--- GPSTracker.Application/Features/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ GPSTracker.Application/Features/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -1,13 +1,26 @@
 ﻿using GPSTracker.Contracts.Responses;
+using GPSTracker.Domain.Entities;
+using GPSTracker.Domain.Repositories;
+using MapsterMapper;
 using MediatR;
 
 namespace GPSTracker.Application.Features.Users.Commands.RegisterUser
 {
     public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, UserDto>
     {
-        public Task<UserDto> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public RegisterUserCommandHandler(IUserRepository userRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var newUser = User.Create(request.Username, request.Email, request.Password);
+            await _userRepository.CreateAsync(newUser);
+            return _mapper.Map<UserDto>(newUser);
         }
     }
 }
