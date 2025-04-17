5d385e35f36ab8709a41a7756c6cc241487ca065
Jayson <oliciajayson@gmail.com>
2025-04-17 16:24:48 +0800

    add EF Core and create GpsTrackerDbContext with base constructor

---
+++ GPSTracker.Infrastructure/Persistence/GpsTrackerDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GPSTracker.Infrastructure.Persistence
+{
+    public class GpsTrackerDbContext : DbContext
+    {
+        public GpsTrackerDbContext(DbContextOptions<GpsTrackerDbContext> options) 
+            : base(options) { }
+        
+    }
+}
