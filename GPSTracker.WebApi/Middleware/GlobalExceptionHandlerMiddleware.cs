f118576cee95a39853e1b49454ae315125dcdf21
Jayson <oliciajayson@gmail.com>
2025-04-17 17:21:05 +0800

    add GlobalExceptionHandlerMiddleware and register in Program.cs

---
+++ GPSTracker.WebApi/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿
+namespace GPSTracker.WebApi.Middleware
+{
+    public class GlobalExceptionHandlerMiddleware : IMiddleware
+    {
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(message: ex.Message);
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(
+                new
+                {
+                    success = false,
+                    message = "An unexpected error occured.",
+                    details = ex.Message
+                });
+
+        }
+    }
+}
--- GPSTracker.WebApi/Program.cs
+++ GPSTracker.WebApi/Program.cs
@@ -1,9 +1,13 @@
+using GPSTracker.WebApi.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<GlobalExceptionHandlerMiddleware>();
 var app = builder.Build();
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
