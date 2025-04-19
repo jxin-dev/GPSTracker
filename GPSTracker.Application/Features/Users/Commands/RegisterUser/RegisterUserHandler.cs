d1f2d5a1f7366f9510bcdf47ae2ea428c34aac6e
Jayson <oliciajayson@gmail.com>
2025-04-19 09:04:08 +0800

    update RegisterUserHandler to use password service

--- GPSTracker.Application/Features/Users/Commands/RegisterUser/RegisterUserHandler.cs
+++ GPSTracker.Application/Features/Users/Commands/RegisterUser/RegisterUserHandler.cs
@@ -1,4 +1,5 @@
-﻿using GPSTracker.Contracts.Responses;
+﻿using GPSTracker.Application.Common.Interfaces;
+using GPSTracker.Contracts.Responses;
 using GPSTracker.Domain.Entities;
 using GPSTracker.Domain.Repositories;
 using MapsterMapper;
@@ -9,16 +10,20 @@ namespace GPSTracker.Application.Features.Users.Commands.RegisterUser
     public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, UserDto>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordService _passwordService;
         private readonly IMapper _mapper;
-        public RegisterUserHandler(IUserRepository userRepository, IMapper mapper)
+
+        public RegisterUserHandler(IUserRepository userRepository, IPasswordService passwordService, IMapper mapper)
         {
             _userRepository = userRepository;
+            _passwordService = passwordService;
             _mapper = mapper;
         }
 
         public async Task<UserDto> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
         {
-            var newUser = User.Create(request.Username, request.Email, request.Password);
+            var newUser = User.Create(request.Username, request.Email);
+            newUser.SetPassword(_passwordService.HashPassword(newUser, request.Password));
             await _userRepository.CreateAsync(newUser);
             return _mapper.Map<UserDto>(newUser);
         }
